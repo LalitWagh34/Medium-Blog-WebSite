e8c9c53a7a3efbf61082599ea718b000664e1c83
unknown <lalitwagh2804@gmail.com>
2024-03-02 22:04:29 +0530

    Initialize the prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,40 @@
 import { Hono } from 'hono'
+import {PrismaClient} from "@prisma/client/edge"
+import {withAccelerate} from "@prisma/extension-accelerate"
+const app = new Hono<{
+    Bindings:{
+        DATABASE_URL:string;
+    }
+}>()
 
-const app = new Hono()
+app.post('/api/v1/signup' ,(c) =>{
+    const prisma = new PrismaClient({
+        datasourceUrl:c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+    return c.text("Hello Hono")
 })
 
+app.post('/api/v1/signin' ,(c) =>{
+    return c.text("Hello Hono")
+})
+
+app.post('/api/v1/blog' ,(c) =>{
+    return c.text("Hello Hono")
+})
+
+app.put('/api/v1/blog' ,(c) =>{
+    return c.text("Hello Hono")
+})
+
+app.get('/api/v1/blog/:id' ,(c) =>{
+    return c.text("Hello Hono")
+})
+
+app.get('/api/v1/blog/bulk' ,(c) =>{
+    return c.text("Hello Hono")
+})
+
+
 export default app
+
